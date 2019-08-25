386766de233ab4ee41a48f8be3eb50948b4dd3db
DESKTOP-8TISN47\mmuro <mmuro010@gmail.com>
2019-08-24 18:11:59 -0700

    Initial

---
+++ listing1-1.py
@@ -0,0 +1,12 @@
+
+#Space_Escape
+#by Sean McManus
+#www.sean.co.uk / www.nostarch.com
+
+WIDTH = 800
+HEIGHT = 600
+player_x = 600
+player_y = 350
+
+def draw():
+    screen.blit(images.backdrop,(0,0))
